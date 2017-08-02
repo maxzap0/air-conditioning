cf29c61adfd5cf831f474b4d783eac0956affce8
Konstantin <z_kos@bk.ru>
2017-08-02 12:25:36 +0300

    UTF-8 еще раз

--- prog05072017/frequency.h
+++ prog05072017/frequency.h
@@ -4,7 +4,7 @@
 
 #include <avr/interrupt.h>
 
-#define	PORT_Freq	PINC		//Порт вентилятора
+#define	PORT_Freq	PINC		//Порт вентилятора 
 #define	PIN_Freq	1			//Порт вентилятора
 
 volatile unsigned int g_seconds = 0;
