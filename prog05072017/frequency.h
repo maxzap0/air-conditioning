5c40ed10a521740c8fe87a7d2f074f1cabddf247
Konstantin <z_kos@bk.ru>
2017-08-08 16:00:10 +0300

    Changed frequency.h
    time_mettering

--- prog05072017/frequency.h
+++ prog05072017/frequency.h
@@ -8,22 +8,20 @@ volatile unsigned int g_seconds = 0;
 volatile unsigned int g_period = 0;
 
 //PIN1
-#define PORT1_freq PINB
-#define PIN1_freq PINB4
+#define PORT1_freq PINC
+#define PIN1_freq PINC0
 
 //PIN2
-#define PORT2_freq PIND
-#define PIN2_freq PIND4
+#define PORT2_freq PINC
+#define PIN2_freq PINC1
 
 //PIN3
-#define PORT3_freq PINB
-#define PIN3_freq PINB5
+#define PORT3_freq PINC
+#define PIN3_freq PINC2
 
 //PIN4
-
 #define PORT4_freq PINC
-#define PIN4_freq PINC0
-
+#define PIN4_freq PINC3
 
 
 //Настройка на подсчет секунд глобальной переменной g_seconds
@@ -53,135 +51,6 @@ ISR(TIMER1_COMPA_vect)
 }
 
 
-//Мгновенный замер импульсов на порте PORT_name и ножке PIN_Freq
-//time_metering - длительность замера
-int freq(volatile uint8_t *PORT_name, uint8_t PINS_freq, unsigned int time_metering)
-{
-	g_seconds = 0;
-	TCNT1 = 0;
-	int frequency=0;
-	int prev=0, real=0;
-	while (g_seconds < time_metering)
-	{
-		real = (*PORT_name & PINS_freq);
-		if (real != prev)
-		{
-			frequency++;
-		}
-		prev = real;
-	}
-	return (int)(frequency/2/time_metering);
-}
-
-
-//Мгновенный замер импульсов на порте PORT_name, на ножках PINS_freq
-//time_metering - длительность замера
-//Выводит импульсы в массив по указателю *frequency соответственно ножкам порта (*frequency[0]=PINx0, *frequency[0]=PINx1,...)
-void freq(int* frequency[], volatile uint8_t *PORT_name, uint8_t PINS_freq, unsigned int time_metering)
-{
-	g_seconds = 0;
-	TCNT1 = 0;
-	int prev[8]={0,};
-	int real[8]={0,};
-	int i=0;
-	char reg_while=0;
-	while (g_seconds < time_metering)
-	{
-		if (reg_while ==0)
-		{
-			for (i=0;i<=7;i++)
-			{
-				*frequency[i]=0;
-			}
-			reg_while=1;
-		}
-
-		for (i=0;i<=7;i++)
-		{
-			if ((PINS_freq>>i)& 1)
-			{
-				real[i] = (*PORT_name & (i+1));
-				if (real[i] != prev[i])
-				{
-					*frequency[i]=*frequency[i]+1;
-				}
-				prev[i] = real[i];
-			}
-		}
-	}
-	if (reg_while==1)
-	{
-		for (i=0;i<=7;i++)
-		{
-			*frequency[i]=(int)(*frequency[i]/2/time_metering);
-		}
-	}
-	
-}
-
-
-//Счетчик импульсов на порте PORT_name и ножке PINS_freq с периодом 
-//period - период замера импульсов в секундах, time_start - время начала замера, time_metering - длительность замера
-int freq(volatile uint8_t *PORT_name, uint8_t PINS_freq, unsigned int time_start, unsigned int time_metering)
-{
-	int frequency=0;
-	int prev=0, real=0;
-	while ((g_seconds >= time_start ) &(g_seconds <= time_start + time_metering))
-	{
-		real = (*PORT_name & PINS_freq);
-		if (real != prev)
-		{
-			frequency++;
-		}
-		prev = real;
-	}
-	return (int)(frequency/2/time_metering);
-}
-
-
-//Счетчик импульсов на порте PORT_name, на ножках PINS_freq
-//time_start - время начала замера, time_metering - длительность замера
-//Выводит импульсы в массив по указателю *frequency соответственно ножкам порта (*frequency[0]=PINx0, *frequency[0]=PINx1,...)
-void freq(int* frequency[], volatile uint8_t *PORT_name, uint8_t PINS_freq, unsigned int time_start, unsigned int time_metering)
-{
-	int prev[8]={0,};
-	int real[8]={0,};
-	int i=0;
-	char reg_while=0;
-	while ((g_seconds >= time_start ) &(g_seconds <= time_start + time_metering))
-	{
-		if (reg_while ==0)
-		{
-			for (i=0;i<=7;i++)
-			{
-				*frequency[i]=0;
-			}
-			reg_while=1;
-		}
-
-		for (i=0;i<=7;i++)
-		{
-			if ((PINS_freq>>i)& 1)
-			{
-				real[i] = (*PORT_name & (i+1));
-				if (real[i] != prev[i])
-				{
-					*frequency[i]=*frequency[i]+1;
-				}
-				prev[i] = real[i];
-			}
-		}
-	}
-	if (reg_while==1)
-	{
-		for (i=0;i<=7;i++)
-		{
-			*frequency[i]=(int)*frequency[i]/2;
-		}
-	}	
-}
-
-
 //Счетчик импульсов на портах PORT1_freq, PORT2_freq, PORT3_freq, PORT4_freq
 // и ножках PIN1_freq, PIN2_freq, PIN3_freq, PIN4_freq
 //time_start - время начала замера, time_metering - длительность замера
@@ -192,7 +61,7 @@ void freq(int* frequency[], unsigned int time_start, unsigned int time_metering)
 	int real[4]={0,};
 	int i=0;
 	char reg_while=0;
-	while ((g_seconds >= time_start ) &(g_seconds <= time_start + time_metering))
+	while ((g_seconds >= time_start ) &(g_seconds < time_start + time_metering))
 	{
 		if (reg_while == 0)
 		{
@@ -240,7 +109,7 @@ void freq(int* frequency[], unsigned int time_start, unsigned int time_metering)
 	{
 		for (i=0;i<=3;i++)
 		{
-			*frequency[i]=(int)*frequency[i]/2;
+			*frequency[i]=(int)((*frequency[i])/2/time_metering);
 		}
 	}
 	
